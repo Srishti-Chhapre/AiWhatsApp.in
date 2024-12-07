9ebe90ca646e5c58dcf7e3c374f6131e85fb0443
Srishti-Chhapre <srishtichhapre@gmail.com>
2024-12-07 23:38:41 +0530

    updated

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 //navbar animation code
 
-const box = document.getElementById('navbar');
-navbar.addEventListener('animationend', () => {
+const box = document.getElementById("navbar");
+navbar.addEventListener("animationend", () => {
   navber.style.animation = "move 1s forwards";
+});
+document.addEventListener("DOMContentLoaded", () => {
+  const navbar = document.querySelector("#navbar");
+  setTimeout(() => {
+    navbar.classList.add("show-logo");
+  }, 100);
+  // Expand navbar after 2 seconds
+  setTimeout(() => {
+    navbar.classList.add("expand");
+  }, 1000);
 });
